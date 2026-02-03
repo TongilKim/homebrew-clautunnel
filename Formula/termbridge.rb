6b0858d1673e0701d93fbbb4d35ed02dd1c8109f
Tongil Kim <tom.k@protopie.io>
2026-02-03 23:43:35 +0900

    Update termbridge to 0.1.7

--- Formula/termbridge.rb
+++ Formula/termbridge.rb
@@ -1,8 +1,8 @@
 class Termbridge < Formula
   desc "Remote monitoring and control for Claude Code CLI"
   homepage "https://github.com/TongilKim/termbridge"
-  url "https://registry.npmjs.org/@tongil_kim/termbridge/-/termbridge-0.1.6.tgz"
-  sha256 "f96820f1899c17671350e1f8318d430eba663ca347403bae77d059b6056a1326"
+  url "https://registry.npmjs.org/@tongil_kim/termbridge/-/termbridge-0.1.7.tgz"
+  sha256 "b7deedcd6c0f64f6af879745c13efabbff8b5024cc6d6ff1448c773f4967f004"
   license "MIT"
 
   depends_on "node"
