573a517ef34b4683947485c1290d6e1f90211a3d
Tongil Kim <35641052+TongilKim@users.noreply.github.com>
2026-02-21 18:52:10 +0900

    chore: Rename formula from termbridge to clautunnel

---
+++ Formula/clautunnel.rb
@@ -0,0 +1,18 @@
+class Clautunnel < Formula
+  desc "Remote monitoring and control for Claude Code CLI"
+  homepage "https://github.com/TongilKim/ClauTunnel"
+  url "https://registry.npmjs.org/@tongil_kim/clautunnel/-/clautunnel-0.1.9.tgz"
+  sha256 "PLACEHOLDER"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/clautunnel", "--version"
+  end
+end
